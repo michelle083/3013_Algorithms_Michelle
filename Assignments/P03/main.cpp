66cf483cf5be9bbc4000cfad4a2722b4e9a73f53
Michelle <100542045+michelle083@users.noreply.github.com>
2023-04-12 12:17:18 -0500

    Update main.cpp

--- Assignments/P03/main.cpp
+++ Assignments/P03/main.cpp
@@ -1,1 +1,32 @@
+/*************************************************************************
+ *
+ *  Author:           Michelle Orru
+ *  Label:            P03
+ *  Title:            Processing in Trie Tree Time
+ *  Course:           CMPS 3013
+ *  Semester:         Spring 2023
+ *
+ *  Description:
+ *        This project is about loading a singly linked list with dictionary
+ *        words, then timing how it takes to look them up.
+ *
+ *  Files:
+ *       main.cpp           : driver program
+ *       termcolor.hpp      : file used to color the text
+ *       timer.hpp          : implementations for timer
+ *       mygetch.hpp        : file to get character, without printing it
+ *       dictionary.txt     : file containing all the dictionary words
+ *
+ *************************************************************************/
 
+#include "termcolor.hpp"
+#include "timer.hpp"
+#include "mygetch.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
